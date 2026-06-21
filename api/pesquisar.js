85974cc20c4a684b24a217adf6f5a5fce90c4734
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-06-21 09:23:09 +0200

    Update pesquisar.js

--- api/pesquisar.js
+++ api/pesquisar.js
@@ -1,8 +1,4 @@
-// =============================
-// PLAYSTAR PESQUISA
-// api/pesquisar.js
-// =============================
-
+// PLAYSTAR TESTE PESQUISA
 
 module.exports = async (req,res)=>{
 
@@ -13,161 +9,51 @@ res.setHeader(
 );
 
 
-
 try{
 
 
 const q =
-req.query.q;
+req.query.q || "Akon";
 
 
 
-if(!q){
+return res.status(200).json({
 
-return res.json({
 sucesso:true,
-resultados:[]
-});
 
-}
+resultados:[
 
-
-
-const url =
-"https://api.cyberhost.online/youtube/search?q="
-+
-encodeURIComponent(q);
-
-
-
-const resposta =
-await fetch(
-url,
 {
-headers:{
-"x-api-key":
-"cyber_f857ee31300990f3451d1a6826f9913b74d52f0a"
+titulo:"Lonely",
+artista:"Akon",
+url:"https://youtube.com/watch?v=test"
+},
+
+{
+titulo:"Right Now",
+artista:"Akon",
+url:"https://youtube.com/watch?v=test2"
+},
+
+{
+titulo:"Smack That",
+artista:"Akon ft Eminem",
+url:"https://youtube.com/watch?v=test3"
 }
-}
-);
 
+],
 
+pesquisa:q
 
-const texto =
-await resposta.text();
-
-
-
-console.log(
-"PESQUISA:",
-texto
-);
-
-
-
-let data;
-
-
-
-try{
-
-data =
-JSON.parse(texto);
-
-}catch(e){
-
-
-return res.status(500).json({
-
-sucesso:false,
-
-erro:
-"API retornou texto inválido",
-
-resposta:texto.substring(0,200)
 
 });
-
-
-}
-
-
-
-
-
-const lista =
-
-data.results ||
-data.data ||
-data.items ||
-[];
-
-
-
-
-
-const resultados = lista.map(m=>({
-
-
-titulo:
-m.title ||
-m.name ||
-"Sem nome",
-
-
-
-artista:
-m.artist ||
-m.author ||
-"Desconhecido",
-
-
-
-url:
-m.url ||
-m.link ||
-m.video_url ||
-m.webpage_url ||
-"",
-
-
-
-imagem:
-m.thumbnail ||
-m.image ||
-""
-
-
-}));
-
-
-
-
-return res.json({
-
-sucesso:true,
-
-resultados
-
-});
-
-
 
 
 
 }catch(e){
 
 
-console.log(
-"ERRO PLAYSTAR:",
-e
-);
-
-
-
 return res.status(500).json({
-
-sucesso:false,
 
 erro:e.message
 
