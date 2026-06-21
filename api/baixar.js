d3002c5a29e9f222b1e65fdcb7a1041c43bed816
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-06-21 08:48:41 +0200

    Refactor baixar.js to use ES6 export syntax

--- api/baixar.js
+++ api/baixar.js
@@ -1,337 +1,118 @@
-// =============================
-// PLAYSTAR API DOWNLOAD
-// api/baixar.js
-// =============================
-
-
-const https = require("https");
-
-
+// PLAYSTAR PESQUISA MUSICA
 
 const API =
 "https://api.cyberhost.online";
-
 
 const KEY =
 "cyber_f857ee31300990f3451d1a6826f9913b74d52f0a";
 
 
+export default async function handler(req,res){
 
 
-
-function cyber(body){
-
-
-return new Promise((resolve,reject)=>{
+try{
 
 
-const data =
-JSON.stringify({
+const q =
+req.query.q;
 
-api_key:KEY,
 
-...body
+if(!q){
 
+return res.json({
+sucesso:false,
+erro:"Digite uma música"
 });
 
+}
 
 
-const req =
-https.request(
 
-
-API + "/youtube/download",
-
-
+const resposta =
+await fetch(
+API+"/youtube/search",
 {
-
 
 method:"POST",
 
-
 headers:{
-
-
-"Content-Type":
-"application/json",
-
-
-"Content-Length":
-Buffer.byteLength(data)
-
-
-}
-
-
+"Content-Type":"application/json"
 },
 
+body:JSON.stringify({
 
+api_key:KEY,
 
-(res)=>{
+q:q,
 
+limit:10
 
-let out="";
-
-
-
-res.on(
-"data",
-chunk=>out+=chunk
-);
-
-
-
-res.on(
-"end",
-()=>{
-
-
-try{
-
-
-resolve(
-JSON.parse(out)
-);
-
-
-
-}catch(e){
-
-
-reject(
-new Error(out)
-);
-
-
-}
-
-
+})
 
 });
-
-
-
-}
-
-
-
-);
-
-
-
-
-
-req.on(
-"error",
-reject
-);
-
-
-
-req.write(data);
-
-
-req.end();
-
-
-
-});
-
-
-
-}
-
-
-
-
-
-
-
-module.exports =
-async(req,res)=>{
-
-
-res.setHeader(
-"Content-Type",
-"application/json"
-);
-
-
-
-try{
-
-
-
-const url =
-req.query.url;
-
-
-
-const tipo =
-req.query.tipo || "audio";
-
-
-
-
-
-if(!url){
-
-
-return res.json({
-
-sucesso:false,
-
-erro:"Cole um link"
-
-});
-
-
-}
-
-
-
 
 
 
 const data =
-await cyber({
-
-
-url,
-
-
-type:
-tipo==="video"
-?
-"video"
-:
-"audio",
+await resposta.json();
 
 
 
-format:
-tipo==="video"
-?
-"mp4"
-:
-"mp3",
-
-
-
-quality:"720"
-
-
-});
-
-
-
-
-
-
-
-
-let file =
-data.file ||
-data.download ||
-data.url;
-
-
-
-
-
-if(!file){
-
-
-return res.json({
-
-
-sucesso:false,
-
-
-erro:"Nenhum arquivo retornado",
-
-
-api:data
-
-
-});
-
-
-}
-
-
-
-
-
-
-if(!file.startsWith("http")){
-
-
-file =
-API + file;
-
-
-}
-
-
-
-
+const musicas =
+data.results ||
+data.data ||
+[];
 
 
 
 res.json({
 
-
 sucesso:true,
 
-
-title:
-data.title ||
-data.filename ||
-"🎵 Música",
+resultados:
+musicas.map(m=>({
 
 
-
-artist:
-data.artist ||
-"PlayStar",
-
+titulo:
+m.title ||
+m.name,
 
 
-download:file
+artista:
+m.artist ||
+m.author ||
+"Desconhecido",
 
 
+url:
+m.url ||
+m.link,
+
+
+capa:
+m.thumbnail ||
+m.image
+
+
+}))
 
 });
-
-
-
-
 
 
 
 }catch(e){
 
 
-console.log(e);
-
-
-
-res.status(500).json({
-
+res.json({
 
 sucesso:false,
 
-
 erro:e.message
 
-
 });
-
 
 
 }
 
 
-
-};
+}
